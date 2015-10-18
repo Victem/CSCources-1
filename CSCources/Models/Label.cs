5a306fde5efe3e632f20b0b30c0036b7a5ecc7d6
Dmitrij-Polyanin <dmitrij.polyanin@gmail.com>
2015-10-18 18:12:56 +0300

    AddedMessagesAndTheadsFirst

---
+++ CSCources/Models/Label.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CSCources.Models
+{
+    public class LabelGroup
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+
+        public ICollection<Label> Labels { get; set; } = new List<Label>();
+    }
+
+    public class Label
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+
+        public int? GroupId { get; set; }
+        public virtual LabelGroup Group { get; set; }
+    }
+}
