af620d8892c9e8aca5aae3062a48c4a2c593c152
Dmitrij-Polyanin <dmitrij.polyanin@gmail.com>
2015-10-18 18:49:13 +0300

    Improoved Message System
    добавлено MessageConfiguration
    И доделаны классы форума

---
+++ CSCources/DAL/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using CSCources.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using CSCources.DAL.EntityConfigurations;
+
+namespace CSCources.DAL
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<File> Files { get; set; }
+        public DbSet<Label> Labels { get; set; }
+        public DbSet<Thread> Threads { get; set; }
+        public DbSet<Message> Messages { get; set; }
+
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new MessageConfiguration());
+         
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- CSCources/Models/Label.cs
+++ CSCources/Models/Label.cs
@@ -5,20 +5,28 @@ using System.Web;
 
 namespace CSCources.Models
 {
+    #region LabelGroup
+    /*
+    /// <summary>
+    /// Для одинаковых тегов по сути но разных по названию, например: программирование и programming
+    /// или ASP.NET и asp.net
+    /// или C# и Си Шарп
+    /// </summary>
     public class LabelGroup
     {
         public int Id { get; set; }
         public string Text { get; set; }
 
         public ICollection<Label> Labels { get; set; } = new List<Label>();
-    }
+    }*/
+    #endregion
 
     public class Label
     {
         public int Id { get; set; }
         public string Text { get; set; }
 
-        public int? GroupId { get; set; }
-        public virtual LabelGroup Group { get; set; }
+        // public int? GroupId { get; set; }
+        // public virtual LabelGroup Group { get; set; }
     }
 }
