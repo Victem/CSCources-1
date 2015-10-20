c698464f5a2bf2c574d6ef5c732bcaf021c10ebc
energiay <energiay@gmail.com>
2015-10-20 18:10:42 +0300

    Вывел на панель всех пользователей и по каждому можно просмотреть роли

---
+++ CSCources/Controllers/AdminController.cs
@@ -0,0 +1,42 @@
+﻿using CSCources.DAL;
+using CSCources.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CSCources.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Admin
+        public ActionResult Index()
+        {
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+            //ViewBag.usr1 = userManager.FindByName("Admin");
+            ViewBag.usr = userManager.Users;
+
+            return View();
+        }
+
+        public PartialViewResult _Settings(string id)
+        {
+            IList<string> roles = new List<string> { "У пользователя нет ролей" };
+            ApplicationUserManager userManager = HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            ApplicationUser usr = userManager.FindById(id);
+            if (usr != null)
+                roles = userManager.GetRoles(usr.Id);
+
+            ViewBag.roles = roles;
+
+            return PartialView();
+        }
+    }
+}
