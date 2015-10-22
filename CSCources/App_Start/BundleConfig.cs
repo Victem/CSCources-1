11da2c1c49d80b62bc6ce618ff5e9cfed4ff20c1
energiay <energiay@gmail.com>
2015-10-22 17:42:14 +0300

    Сделано всплывающее окно при помощм jquery-iu Профиль/роль любого пользователя будут отоброжаться именно там.

--- CSCources/App_Start/BundleConfig.cs
+++ CSCources/App_Start/BundleConfig.cs
@@ -26,6 +26,10 @@ namespace CSCources
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/jquery-ui-{version}.js",
+                         "~/Scripts/JavaScriptPopup.js"));
         }
     }
 }
---
+++ CSCources/Controllers/ProfilUserController.cs
@@ -0,0 +1,41 @@
+﻿using CSCources.DAL;
+using CSCources.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CSCources.Controllers
+{
+    public class ProfilUserController : Controller
+    {
+
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: ProfilUser
+        public ActionResult Index()
+        {
+            //получаем всех пользователей
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+            ViewBag.Usrs = userManager.Users;
+
+            return View();
+        }
+
+        public ActionResult _GetUsrs(string id)
+        {
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+            var thisUser = userManager.FindById(id);
+
+            if (thisUser != null)
+            {
+                return PartialView("_GetUsrs", thisUser);
+            }
+
+            return View("Index");
+        }
+    }
+}
